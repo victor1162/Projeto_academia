a0a425331d0c79b8556926bb67f14712313c6bbb
Victor santos <victor_santos1162@hotmail.com>
2021-04-23 10:03:42 -0300

    Academia

---
+++ js/alimentacao.js
@@ -0,0 +1,141 @@
+/***fazendo aplicação rolagem para baixo menu***/
+
+/***Final da aplicação***/
+
+/* modoal suplementação**/
+function abrirModal(){
+	document.getElementById('modal').style.top = '0'
+
+	}
+function fecharModal(){
+	document.getElementById('modal').style.top = '-100%'
+
+	}
+
+
+
+
+
+
+function abrirModal2(){
+	document.getElementById('modal2').style.top = '0'
+
+	}
+function fecharModal2(){
+	document.getElementById('modal2').style.top = '-100%'
+
+	}
+
+
+
+
+
+
+function abrirModal3(){
+	document.getElementById('modal3').style.top = '0'
+
+	}
+function fecharModal3(){
+	document.getElementById('modal3').style.top = '-100%'
+
+	}
+
+
+
+
+
+
+function abrirModal4(){
+	document.getElementById('modal4').style.top = '0'
+
+	}
+function fecharModal4(){
+	document.getElementById('modal4').style.top = '-100%'
+
+	}
+
+
+
+
+
+function abrirModal5(){
+	document.getElementById('modal5').style.top = '0'
+
+	}
+function fecharModal5(){
+	document.getElementById('modal5').style.top = '-100%'
+
+	}
+
+
+
+
+
+
+function abrirModal6(){
+	document.getElementById('modal6').style.top = '0'
+
+	}
+function fecharModal6(){
+	document.getElementById('modal6').style.top = '-100%'
+
+	}
+
+
+
+
+
+
+function abrirModal7(){
+	document.getElementById('modal7').style.top = '0'
+
+	}
+function fecharModal7(){
+	document.getElementById('modal7').style.top = '-100%'
+
+	}
+
+
+
+
+
+
+
+
+function abrirModal8(){
+	document.getElementById('modal8').style.top = '0'
+
+	}
+function fecharModal8(){
+	document.getElementById('modal8').style.top = '-100%'
+
+	}
+
+
+
+
+
+
+
+function abrirModal9(){
+	document.getElementById('modal9').style.top = '0'
+
+	}
+function fecharModal9(){
+	document.getElementById('modal9').style.top = '-100%'
+
+	}
+
+
+
+
+
+
+function abrirModal10(){
+	document.getElementById('modal10').style.top = '0'
+
+	}
+function fecharModal10(){
+	document.getElementById('modal10').style.top = '-100%'
+
+	}
